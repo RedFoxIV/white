0422436278de3d81b8b697964ed7ee769f7f1065
RedFoxIV <38788538+RedFoxIV@users.noreply.github.com>
2022-08-24 03:27:57 +0300

    антивалера

--- lua/SS13.lua
+++ lua/SS13.lua
@@ -1,11 +1,12 @@
 local SS13 = {}
 
-SS13.SSlua = dm.global_vars.vars.SSlua
+SS13.SSlua = dm.global_vars:get_var("SSlua")
 
 SS13.global_proc = "some_magic_bullshit"
 
-for _, state in SS13.SSlua.vars.states do
-	if state.vars.internal_id == dm.state_id then
+local states = SS13.SSlua:get_var("states"):to_table()
+for _, state in states do
+	if state:get_var("internal_id") == dm.state_id then
 		SS13.state = state
 		break
 	end
@@ -17,7 +18,7 @@ end
 
 function SS13.new(type, ...)
 	local datum = dm.global_proc("_new", type, { ... })
-	local references = SS13.state.vars.references
+	local references = SS13.state:get_var("references")
 	references:add(datum)
 	return datum
 end
@@ -30,21 +31,18 @@ function SS13.await(thing_to_call, proc_to_call, ...)
 		proc_to_call = "/proc/" .. proc_to_call
 	end
 	local promise = SS13.new("/datum/auxtools_promise", thing_to_call, proc_to_call, ...)
-	local promise_vars = promise.vars
-	while promise_vars.status == 0 do
+	while promise:get_var("status") == 0 do
 		sleep()
 	end
-	local return_value, runtime_message = promise_vars.return_value, promise_vars.runtime_message
-	dm.global_proc("qdel", promise)
-	return return_value, runtime_message
+	return promise:get_var("return_value"), promise:get_var("runtime_message")
 end
 
 function SS13.wait(time, timer)
-	local callback = SS13.new("/datum/callback", SS13.SSlua, "queue_resume", SS13.state, __next_yield_index)
+	local index = #__yield_table + 1
+	local callback = SS13.new("/datum/callback", SS13.SSlua, "queue_resume", SS13.state, index)
 	local timedevent = dm.global_proc("_addtimer", callback, time * 10, 8, timer, debug.info(1, "sl"))
 	coroutine.yield()
 	dm.global_proc("deltimer", timedevent, timer)
-	dm.global_proc("qdel", callback)
 end
 
 function SS13.register_signal(datum, signal, func, make_easy_clear_function)
@@ -54,24 +52,26 @@ function SS13.register_signal(datum, signal, func, make_easy_clear_function)
 	if not SS13.istype(datum, "/datum") then
 		return
 	end
-	if not SS13.signal_handlers[datum] then
-		SS13.signal_handlers[datum] = {}
+	local ref = dm.global_proc("REF", datum)
+	if not SS13.signal_handlers[ref] then
+		SS13.signal_handlers[ref] = {}
 	end
 	if signal == "_cleanup" then
 		return
 	end
-	if not SS13.signal_handlers[datum][signal] then
-		SS13.signal_handlers[datum][signal] = {}
+	if not SS13.signal_handlers[ref][signal] then
+		SS13.signal_handlers[ref][signal] = {}
 	end
 	local callback = SS13.new("/datum/callback", SS13.state, "call_function_return_first")
 	callback:call_proc("RegisterSignal", datum, signal, "Invoke")
-	local path = { "SS13", "signal_handlers", datum, signal, callback, "func" }
-	callback.vars.arguments = { path }
-	if not SS13.signal_handlers[datum]["_cleanup"] then
-		local cleanup_path = { "SS13", "signal_handlers", datum, "_cleanup", "func" }
+	local callback_ref = dm.global_proc("REF", callback)
+	local path = { "SS13", "signal_handlers", ref, signal, callback_ref, "func" }
+	callback:set_var("arguments", { path })
+	if not SS13.signal_handlers[ref]["_cleanup"] then
+		local cleanup_path = { "SS13", "signal_handlers", ref, "_cleanup", "func" }
 		local cleanup_callback = SS13.new("/datum/callback", SS13.state, "call_function_return_first", cleanup_path)
 		cleanup_callback:call_proc("RegisterSignal", datum, "parent_qdeleting", "Invoke")
-		SS13.signal_handlers[datum]["_cleanup"] = {
+		SS13.signal_handlers[ref]["_cleanup"] = {
 			func = function(datum)
 				SS13.signal_handler_cleanup(datum)
 				dm.global_proc("qdel", cleanup_callback)
@@ -80,21 +80,22 @@ function SS13.register_signal(datum, signal, func, make_easy_clear_function)
 		}
 	end
 	if signal == "parent_qdeleting" then --We want to make sure that the cleanup function is the very last signal handler called.
-		local comp_lookup = datum.vars.comp_lookup
+		local comp_lookup = datum:get_var("comp_lookup")
 		if comp_lookup then
-			local lookup_for_signal = comp_lookup.entries.parent_qdeleting
+			local lookup_table = comp_lookup:to_table()
+			local lookup_for_signal = lookup_table.parent_qdeleting
 			if lookup_for_signal and not SS13.istype(lookup_for_signal, "/datum") then
 				local cleanup_callback_index =
-					dm.global_proc("_list_find", lookup_for_signal, SS13.signal_handlers[datum]["_cleanup"].callback)
-				if cleanup_callback_index ~= 0 and cleanup_callback_index ~= #comp_lookup then
-					dm.global_proc("_list_swap", lookup_for_signal, cleanup_callback_index, #lookup_for_signal)
+					dm.global_proc("_list_find", lookup_for_signal, SS13.signal_handlers[ref]["_cleanup"].callback)
+				if cleanup_callback_index ~= 0 then
+					dm.global_proc("_list_swap", lookup_for_signal, cleanup_callback_index, lookup_for_signal.len)
 				end
 			end
 		end
 	end
-	SS13.signal_handlers[datum][signal][callback] = { func = func, callback = callback }
+	SS13.signal_handlers[ref][signal][callback_ref] = { func = func, callback = callback }
 	if make_easy_clear_function then
-		local clear_function_name = "clear_signal_" .. tostring(datum) .. "_" .. signal .. "_" .. tostring(callback)
+		local clear_function_name = "clear_signal_" .. ref .. "_" .. signal .. "_" .. callback_ref
 		SS13[clear_function_name] = function()
 			if callback then
 				SS13.unregister_signal(datum, signal, callback)
@@ -122,34 +123,36 @@ function SS13.unregister_signal(datum, signal, callback)
 		return
 	end
 
-	local function clear_easy_clear_function(callback_to_clear)
-		local clear_function_name = "clear_signal_" .. tostring(datum) .. "_" .. signal .. "_" .. tostring(callback_to_clear)
+	local ref = dm.global_proc("REF", datum)
+	local function clear_easy_clear_function(callback_ref)
+		local clear_function_name = "clear_signal_" .. ref .. "_" .. signal .. "_" .. callback_ref
 		SS13[clear_function_name] = nil
 	end
 
-	if not SS13.signal_handlers[datum] then
+	if not SS13.signal_handlers[ref] then
 		return
 	end
 	if signal == "_cleanup" then
 		return
 	end
-	if not SS13.signal_handlers[datum][signal] then
+	if not SS13.signal_handlers[ref][signal] then
 		return
 	end
 
 	if not callback then
-		for handler_key, handler_info in SS13.signal_handlers[datum][signal] do
+		for handler_key, handler_info in SS13.signal_handlers[ref][signal] do
 			clear_easy_clear_function(handler_key)
 			clear_handler(handler_info)
 		end
-		SS13.signal_handlers[datum][signal] = nil
+		SS13.signal_handlers[ref][signal] = nil
 	else
 		if not SS13.istype(callback, "/datum/callback") then
 			return
 		end
-		clear_easy_clear_function(callback)
-		clear_handler(SS13.signal_handlers[datum][signal][callback])
-		SS13.signal_handlers[datum][signal][callback] = nil
+		local callback_ref = dm.global_proc("REF", callback)
+		clear_easy_clear_function(callback_ref)
+		clear_handler(SS13.signal_handlers[ref][signal][callback_ref])
+		SS13.signal_handlers[ref][signal][callback_ref] = nil
 	end
 end
 
@@ -157,31 +160,33 @@ function SS13.signal_handler_cleanup(datum)
 	if not SS13.signal_handlers then
 		return
 	end
-	if not SS13.signal_handlers[datum] then
+	local ref = dm.global_proc("REF", datum)
+	if not SS13.signal_handlers[ref] then
 		return
 	end
 
-	for signal, _ in SS13.signal_handlers[datum] do
+	for signal, _ in SS13.signal_handlers[ref] do
 		SS13.unregister_signal(datum, signal)
 	end
 
-	SS13.signal_handlers[datum] = nil
+	SS13.signal_handlers[ref] = nil
 end
 
-function SS13.set_timeout(time, func, timer)
+function SS13.set_timeout(time, func)
 	if not SS13.timeouts then
 		SS13.timeouts = {}
 	end
 	local callback = SS13.new("/datum/callback", SS13.state, "call_function")
-	local timedevent = dm.global_proc("_addtimer", callback, time * 10, 8, timer, debug.info(1, "sl"))
-	SS13.timeouts[callback] = function()
-		SS13.timeouts[callback] = nil
-		dm.global_proc("deltimer", timedevent, timer)
-		dm.global_proc("qdel", callback)
+	local callback_ref = dm.global_proc("REF", callback)
+	SS13.timeouts[callback_ref] = function()
+		SS13.timeouts[callback_ref] = nil
 		func()
 	end
-	local path = { "SS13", "timeouts", callback }
-	callback.vars.arguments = { path }
+	local path = { "SS13", "timeouts", callback_ref }
+	callback:set_var("arguments", { path })
+	dm.global_proc("_addtimer", callback, time * 10, 8, nil, debug.info(1, "sl"))
 end
 
 return SS13
+
+
